2baf97284d8a3c560a3d34e04e344ee9d5c1fe2e
John Larence Lusaya <150530035+Larence2005@users.noreply.github.com>
2024-10-04 15:25:31 +0800

    Minor Changes

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ import altair as alt
 from wordcloud import WordCloud
 from mpl_toolkits.mplot3d import Axes3D
 
-st.title('Analyzing Laptops and Their Prices\n\n\n')
+st.title('Analyzing Laptops and Their Prices')
+st.write('')
+st.write('')
 
 "## GROUP 4 - BM3"
 
 st.write("The purpose of this is to convert our previous Data Visualization activity in Google Colab Notebook into a Streamlit Web Application")
-st.write("Also check the GitHub Repository for the source code")
-
+st.markdown("Also check the [GitHub Repository](https://github.com/Larence2005/Group-4-Laptop-Prices) for the source code.")
+st.write('')
 "# Describing the Dataset"
 
 df = pd.read_csv("laptop_price - dataset.csv")
